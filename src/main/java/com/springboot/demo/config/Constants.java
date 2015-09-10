b0a4d76d48c8676549dcfefb21b2cda28a6af1a2
Himanshu Virmani <himanshuvirmani@gmail.com>
2015-09-10 17:46:46 +0530

    first commit..Basic libraries added. yet to make add resources and other stuff.

---
+++ src/main/java/com/springboot/demo/config/Constants.java
@@ -0,0 +1,15 @@
+package com.springboot.demo.config;
+
+/**
+ * Application constants.
+ */
+public final class Constants {
+
+  // Spring profile for development, production and "fast", see http://jhipster.github.io/profiles.html
+  public static final String SPRING_PROFILE_DEVELOPMENT = "dev";
+  public static final String SPRING_PROFILE_PRODUCTION = "prod";
+  public static final String SPRING_PROFILE_FAST = "fast";
+
+  private Constants() {
+  }
+}
---
+++ src/main/java/com/springboot/demo/config/apidoc/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Swagger api specific code.
+ */
+package com.springboot.demo.config.apidoc;
