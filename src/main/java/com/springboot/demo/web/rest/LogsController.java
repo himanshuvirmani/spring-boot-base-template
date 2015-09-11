14093a958863e0e16c562f7e080cfd3c4183f13b
Himanshu Virmani <himanshu.virmani@flipkart.com>
2015-09-11 16:37:22 +0530

    added Spring Data JPA..

--- src/main/java/com/springboot/demo/web/rest/LogsController.java
+++ src/main/java/com/springboot/demo/web/rest/LogsController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api")
-public class LogsResource {
+public class LogsController {
 
     @RequestMapping(value = "/logs",
             method = RequestMethod.GET,
