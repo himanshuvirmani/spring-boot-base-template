a0289300984dee22d30396461f31934c1b58ccb9
Himanshu Virmani <himanshuvirmani@gmail.com>
2015-09-11 16:37:22 +0530

    added Spring Data JPA..

---
+++ src/main/java/com/springboot/demo/repository/HotelRepository.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2012-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.springboot.demo.repository;
+
+import com.springboot.demo.domain.City;
+import com.springboot.demo.domain.Hotel;
+import com.springboot.demo.domain.HotelSummary;
+import com.springboot.demo.domain.RatingCount;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
+
+public interface HotelRepository extends Repository<Hotel, Long> {
+
+	Hotel findByCityAndName(City city, String name);
+
+	@Query("select new com.springboot.demo.domain.HotelSummary(h.city, h.name, avg(r.rating)) "
+			+ "from Hotel h left outer join h.reviews r where h.city = ?1 group by h")
+	Page<HotelSummary> findByCity(City city, Pageable pageable);
+
+	@Query("select new com.springboot.demo.domain.RatingCount(r.rating, count(r)) "
+			+ "from com.springboot.demo.domain.Review r where r.hotel = ?1 group by r.rating order by r.rating DESC")
+	List<RatingCount> findRatingCounts(Hotel hotel);
+}
